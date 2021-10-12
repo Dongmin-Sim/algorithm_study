f34d9c52c64ae1362c7a2fb4aa9f96a298774e67
Hyebin Park <78071068+hbhb0311@users.noreply.github.com>
2021-10-12 10:53:46 +0900

    이모티콘

---
+++ 3주차/HyebinPark/14226.py
@@ -0,0 +1,27 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+s = int(input())
+graph = dict()
+q = deque()
+q.append((1, 0))
+graph[(1, 0)] = 0
+
+while q:
+    screen, clip = q.popleft()
+    if screen == s:
+        print(graph[(screen, clip)])
+        break
+
+    if (screen, screen) not in graph.keys():
+        graph[(screen, screen)] = graph[(screen, clip)] + 1
+        q.append((screen, screen))
+
+    if (screen + clip, clip) not in graph.keys():
+        graph[(screen + clip, clip)] = graph[(screen, clip)] + 1
+        q.append((screen + clip, clip))
+
+    if (screen - 1, clip) not in graph.keys():
+        graph[(screen - 1, clip)] = graph[(screen, clip)] + 1
+        q.append((screen - 1, clip))
